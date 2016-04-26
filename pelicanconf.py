b8098b5e8b196916e9a06ee70934c1c6b4539e4e
chrisittner <mail@chrisittner.de>
2016-04-26 12:31:30 +0200

    initial commit (3)

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,7 +7,7 @@ SITENAME = 'Structure Learning for pgmpy'
 SITEURL = ''
 
 PATH = 'content'
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Europe/Berlin'
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
@@ -16,6 +16,22 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+
+
+# get date and slug from filename, rather than only the date. By default slug is derived from 'title'.
+DEFAULT_CATEGORY = 'pgmpy'
+FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2})-(?P<slug>.*)'
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+PAGE_URL = 'pages/{slug}.html'
+PAGE_SAVE_AS = 'pages/{slug}.html'
+TAG_URL = None
+TAG_SAVE_AS = None
+AUTHOR_URL = ''
+AUTHOR_SAVE_AS = ''
+YEAR_ARCHIVE_SAVE_AS = None
+MONTH_ARCHIVE_SAVE_AS = None
+DAY_ARCHIVE_SAVE_AS = None
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
@@ -31,3 +47,21 @@ DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+PLUGIN_PATHS = ['./plugins']
+PLUGINS = ['extract_toc', 'render_math', 'better_figures_and_images', ]  # 'md_yaml']
+MD_EXTENSIONS = ['codehilite', 'extra', 'smarty', 'toc']
+
+STATIC_PATHS = ['extra', 'images', 'pdfs']
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+    'extra/htaccess': {'path': '.htaccess'}
+}
+
+THEME = 'themes/bootstrap'  # pelican-blue or Flex (nice-blog, twenty-html5up, html5-dopetrope, free-agent)
+
+
+COLOPHON = True
+COLOPHON_TITLE = 'About'
+COLOPHON_CONTENT = "Mainly...."
